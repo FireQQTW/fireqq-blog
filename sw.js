28751541e17301a196b9fc63050584fae3949c21
FireQQTW <azraelxuan927@gmail.com>
2021-05-20 19:34:12 +0000

    Deploying to gh-pages from @ FireQQTW/fireqq-blog@b8986bcf82708c51df624d063d3fdacf1d75af3a 🚀

--- sw.js
+++ sw.js
@@ -42,11 +42,11 @@ self.__precacheManifest = [
     "url": "dc6a8720040df98778fe970bf6c000a41750d3ae-c834ff89941df20a8e4f.js"
   },
   {
-    "url": "app-fec521ad16950169a90e.js"
+    "url": "app-72af1d6b5cba3e9cad5d.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "50aaa89ea9101b7f36ec9ae1104e4502"
+    "revision": "21141dffaff44cc6b6e9dbfaa5a156c9"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
@@ -148,7 +148,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-fec521ad16950169a90e.js`))) {
+  if (!resources || !(await caches.match(`/app-72af1d6b5cba3e9cad5d.js`))) {
     return await fetch(event.request)
   }
 
